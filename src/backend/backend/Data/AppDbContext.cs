6b69d3bbeaf070de43d50b050a7c493738265204
Arthur Coelho de Souza <51198520+arthurcoelhosouza@users.noreply.github.com>
2026-04-25 23:41:42 -0300

    Ajustes nas referências do projeto

--- src/backend/backend/Data/AppDbContext.cs
+++ src/backend/backend/Data/AppDbContext.cs
@@ -13,6 +13,7 @@ namespace backend.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<EmotionLog> Emotions { get; set; }
+        public DbSet<BreathingLog> BreathingLogs { get; set; }
         public DbSet<UserOnboarding> UserOnboardings { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
